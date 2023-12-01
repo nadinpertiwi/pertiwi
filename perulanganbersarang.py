aa682d6d61b39e4c8e15cdb32a0d03143a74541c
nadinpertiwii <152601786+nadinpertiwi@users.noreply.github.com>
2023-12-01 16:42:46 +0700

    nadincantikk

---
+++ perulanganbersarang.py
@@ -0,0 +1,12 @@
+
+
+
+# for i in range (1, 2) :
+#     print(I)
+#     for j in range (1,6) :
+#         print(i ,"+", j, "=", int(i * j))
+
+kumpulanBarang = [ ["meja","kursi","tatakan",],["monitor","TV","Handphone"] ]
+for barang in kumpulanBarang :
+    for item in barang :
+        print(item,end=",").__annotations__8
